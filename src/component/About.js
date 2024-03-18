85b8c86b07776ba8eba90cb1f2a06e52126016f3
hyeopsang <153697520+hyeopsang@users.noreply.github.com>
2024-03-19 04:33:25 +0900

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -41,10 +41,10 @@ const About = ({useScrollFadeIn, useScrollFadeIn2}) => {
                 </div>
                 <div className="about_txt" onMouseLeave={handleMouseLeave} {...animatedItem2} >
                     <p onMouseEnter={handleMouseEnter}>
-                        "저는 새로운 것을 배우는 것을 즐기며 끊임없이 발전하고자 하는 열정을 가지고 있습니다. 지속적인 학습과 도전을 통해 자신의 잠재력을 최대한 발휘하고 싶습니다." "
+                        " 저는 새로운 것을 배우는 것을 즐기며 끊임없이 발전하고자 하는 열정을 가지고 있습니다. 지속적인 학습과 도전을 통해 자신의 잠재력을 최대한 발휘하고 싶습니다. "
                     </p>
                     <p onMouseEnter={handleMouseEnter2}>
-                        "저는 피드백을 소중히 받아들이고 지속적인 개선을 통해 더 나은 결과물을 만들어내는 것을 중요시합니다. 열린 마음으로 다양한 의견을 수용하며, 팀과 함께 성장하고 발전하기 위해 노력합니다.""
+                        " 저는 피드백을 소중히 받아들이고 지속적인 개선을 통해 더 나은 결과물을 만들어내는 것을 중요시합니다. 열린 마음으로 다양한 의견을 수용하며, 팀과 함께 성장하고 발전하기 위해 노력합니다. "
                     </p>
                 </div>
             </div>
